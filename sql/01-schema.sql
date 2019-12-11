316414d36c8e938c30c987657d70b324ada6ee73
Clément DOUIN <soywod@users.noreply.github.com>
2019-12-11 19:06:37 +0100

    improve screens model

--- sql/01-schema.sql
+++ sql/01-schema.sql
@@ -1,10 +1,3 @@
-CREATE SCHEMA IF NOT EXISTS public;
-REVOKE ALL ON ALL TABLES IN SCHEMA public FROM public;
-
-CREATE ROLE authenticator NOINHERIT LOGIN PASSWORD 'password';
-CREATE ROLE anon NOLOGIN;
-CREATE ROLE bo_user NOLOGIN;
-
 SELECT pg_catalog.set_config('search_path', '', false);
 SET check_function_bodies = false;
 SET client_encoding = 'UTF8';
@@ -18,17 +11,25 @@ SET standard_conforming_strings = on;
 SET statement_timeout = 0;
 SET xmloption = content;
 
---
--- Name: insert_pending_pairing(); Type: FUNCTION; Schema: public; Owner: user
---
 
-CREATE FUNCTION public.insert_pending_pairing() RETURNS character
+CREATE SCHEMA IF NOT EXISTS public;
+REVOKE ALL ON ALL TABLES IN SCHEMA public FROM public;
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp" SCHEMA pg_catalog;
+
+
+CREATE ROLE authenticator NOINHERIT LOGIN PASSWORD 'password';
+CREATE ROLE anon NOLOGIN;
+CREATE ROLE bo_user NOLOGIN;
+
+
+CREATE FUNCTION public.declare_new_screen() RETURNS character
     LANGUAGE plpgsql
     AS $$
 DECLARE
     ascii_numbers integer[];
     ascii_letters integer[];
     ascii_chars integer[];
+    id uuid;
     code char(5);
 BEGIN
     ascii_numbers := array(select * from generate_series(48, 57));
@@ -36,42 +37,29 @@ BEGIN
     ascii_chars := ascii_numbers || ascii_letters;
 
     LOOP
+        SELECT uuid_generate_v4() INTO id;
         SELECT array_to_string(array(
           SELECT chr(ascii_chars[round(random() * (array_length(ascii_chars, 1) - 1)) + 1] :: integer)
           FROM generate_series(1, 5))
         , '')
         INTO code;
         BEGIN
-            INSERT INTO pending_pairings (code) VALUES (code);
-            RETURN code;
+            INSERT INTO screens (id, code) VALUES (id, code);
+            RETURN json_build_object('id', id, 'code', code);
         EXCEPTION WHEN unique_violation THEN
         END;
     END LOOP;
 END;
 $$;
 
+ALTER FUNCTION public.declare_new_screen() OWNER TO "authenticator";
 
-ALTER FUNCTION public.insert_pending_pairing() OWNER TO "authenticator";
 
---
--- Name: pending_pairings; Type: TABLE; Schema: public; Owner: user
---
-
-CREATE TABLE public.pending_pairings (
-    code character(5) NOT NULL,
+CREATE TABLE public.screens (
+    id uuid NOT NULL PRIMARY KEY,
+    code character(5) NOT NULL UNIQUE,
+    user_id character varying(256) NULL DEFAULT NULL,
     created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
-
-ALTER TABLE public.pending_pairings OWNER TO "authenticator";
-
---
--- Name: pending_pairings pending-pairings-unique; Type: CONSTRAINT; Schema: public; Owner: user
---
-
-ALTER TABLE ONLY public.pending_pairings
-    ADD CONSTRAINT "pending-pairings-unique" UNIQUE (code);
-
---
--- PostgreSQL database dump complete
---
+ALTER TABLE public.screens OWNER TO "authenticator";
--- sql/02-roles.sql
+++ sql/02-roles.sql
@@ -1,7 +1,7 @@
 GRANT anon TO authenticator;
 GRANT USAGE ON SCHEMA public TO anon;
-GRANT EXECUTE ON FUNCTION insert_pending_pairing() TO anon;
-GRANT INSERT ON pending_pairings TO anon;
+GRANT EXECUTE ON FUNCTION declare_new_screen() TO anon;
+GRANT INSERT ON screens TO anon;
 
 GRANT bo_user TO authenticator;
 GRANT USAGE ON SCHEMA public TO bo_user;
